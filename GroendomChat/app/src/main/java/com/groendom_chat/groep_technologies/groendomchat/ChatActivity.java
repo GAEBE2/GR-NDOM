ea7554dcc34b20ee54a47dc74f98496d63e1cccf
Patrick Wissiak <pwissiak@hispeed.ch>
2017-08-28 13:06:34 +0200

    Added android project.

---
+++ GroendomChat/app/src/main/java/com/groendom_chat/groep_technologies/groendomchat/ChatActivity.java
@@ -0,0 +1,14 @@
+package com.groendom_chat.groep_technologies.groendomchat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_activity);
+    }
+}
