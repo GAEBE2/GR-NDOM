a18cbc1e5fbfaac21a03d57cec853babf654ea9c
Patrick Wissiak <pwissiak@hispeed.ch>
2017-09-18 14:45:13 +0200

    Created chat bubbles.

--- GroendomChat/app/src/main/java/com/groendom_chat/groep_technologies/groendomchat/activities/ChatActivity.java
+++ GroendomChat/app/src/main/java/com/groendom_chat/groep_technologies/groendomchat/activities/ChatActivity.java
@@ -3,8 +3,17 @@ package com.groendom_chat.groep_technologies.groendomchat.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
 
 import com.groendom_chat.groep_technologies.groendomchat.R;
+import com.groendom_chat.groep_technologies.groendomchat.model.Message;
+import com.groendom_chat.groep_technologies.groendomchat.model.MessageHandler;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class ChatActivity extends Activity {
 
@@ -16,5 +25,14 @@ public class ChatActivity extends Activity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("HIIOOO");
 
+        MessageHandler messageHandler = new MessageHandler(getApplicationContext());
+        LinearLayout layout = (LinearLayout) findViewById(R.id.chat_activity_content);
+
+        for (int i = 1; i < 10; i++) {
+            messageHandler.createOut(layout, new Message("Hallo" + i));
+        }
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -1);
+        messageHandler.createOut(layout, new Message("HIOOOooooooooooooooooooooooooooooooooooooo", cal.getTime()));
     }
 }
